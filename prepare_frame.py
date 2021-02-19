456f97d3b658ed62b758da7315361195143f948f
zhaohoj <1072451355@qq.com>
2021-02-19 20:40:13 +0800

    commit

--- prepare_frame.py
+++ prepare_frame.py
@@ -1,25 +1,51 @@
 import cv2
 import numpy as np
+import queue
+import logging
 
-filepath = '/home/zhaohoj/Videos/龙门客栈-withSTTN.mp4'
-cap = cv2.VideoCapture(filepath)
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-fps = cap.get(cv2.CAP_PROP_FPS)
-total_cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-# fourcc = cap.get(cv2.CAP_PROP_FOURCC)
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
 
-while True:
-    ret, frame = cap.read()
-    if ret:
-        out.write(frame)
-    else:
-        break
-cap.release()
-out.release()
+# filepath = '/home/zhaohoj/Videos/龙门客栈-withSTTN.mp4'
+# cap = cv2.VideoCapture(filepath)
+# width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# fps = cap.get(cv2.CAP_PROP_FPS)
+# total_cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+# # fourcc = cap.get(cv2.CAP_PROP_FOURCC)
+# fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+#
+# out = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
+#
+# while True:
+#     ret, frame = cap.read()
+#     if ret:
+#         out.write(frame)
+#     else:
+#         break
+# cap.release()
+# out.release()
 
-def readFrame():
-    pass
+
+def readFrame(input_path, outputQ: queue):
+    cap = cv2.VideoCapture(input_path)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    total_cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    _inputCnt = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            outputQ.put('EOF')
+            break
+        qElem = {
+            'index': _inputCnt,
+            'decFrm': frame
+        }
+        _inputCnt += 1
+        outputQ.put(qElem)
+    # end
+    logger.info('end readFrame')
+    cap.release()
--- resize_frame.py
+++ resize_frame.py
@@ -1,2 +1,33 @@
-def resizeFrame():
-    pass
+import queue
+import numpy as np
+import torch
+import logging
+import cv2
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def resizeFrame(inputQ, outputQ):
+    while True:
+        try:
+            qElem = inputQ.get(timeout=0.1)
+        except TimeoutError:
+            continue
+        except queue.Empty:
+            continue
+        if qElem == 'EOF':
+            break
+        try:
+            calcDepthWidth = 512
+            calcDepthHeight = 288
+            decFrm = qElem['decFrm']
+            calcDepthFrm = cv2.resize(decFrm, (calcDepthWidth, calcDepthHeight))
+            calcDepthTen = torch.from_numpy(np.asarray(calcDepthFrm))
+            qElem['calcDepthTen'] = calcDepthTen
+            logger.info(f'Resize frame {qElem["index"]}')
+            outputQ.put(qElem)
+        except Exception:
+            import traceback
+            traceback.print_exc()
+    outputQ.put("EOF")
