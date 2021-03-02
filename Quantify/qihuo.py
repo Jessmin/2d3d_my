dc29c58f59d16cd71d95b4e26aadc4fb07d26112
zhaohoj <zhaohongjie@hualumedia.com>
2021-03-02 13:50:24 +0800

    commit

--- Quantify/qihuo.py
+++ Quantify/qihuo.py
@@ -1,16 +1,17 @@
-dict = {
-    "白银期货": ("AG2106", "SHF")
-}
-push_users = {
-    "self": "kyx6kNQZtoN8XkQXmjxyO1eMD",
-    "fws": "g4Elz5ql3N8ZPgItJ0VVWdaah",
-}
 import requests
 import json
 import tushare as ts
 import os
 import numpy as np
 import datetime
+
+dict = {
+    "白银期货": ("AG2106", "SHF")
+}
+push_users = {
+    "self": "kyx6kNQZtoN8XkQXmjxyO1eMD",
+    "冯文颂": "g4Elz5ql3N8ZPgItJ0VVWdaah",
+}
 
 # step 1
 ts.set_token('1e5aab687e73d645eba8d98be1a0f0c4d83555783adf1c5a7917a3dc')
@@ -40,12 +41,14 @@ def get_ma60(ts_code, today, saved_ma60_file):
 def get_latest_5min(url):
     r = requests.get(url).content
     js = json.loads(r)
+    if len(js) < 1:
+        return None
     return js[0]
 
 
 def calc(ma60, latest_5min):
-    # print(ma60)
-    # print(latest_5min)
+    print(ma60)
+    print(latest_5min)
     low = latest_5min[3]
     close = latest_5min[4]
     # print(latest_5min['day'])
@@ -57,8 +60,8 @@ def calc(ma60, latest_5min):
     # low = latest_5min['low']
     # close = latest_5min['close']
     if float(low) < float(ma60) < float(close):
-        return True
-    return False
+        return True, f'ma60:{ma60},latest_5min:{latest_5min}'
+    return True, None
 
 
 def pushWechat(title, content):
@@ -76,6 +79,7 @@ def main():
     today = datetime.date.today()
     saved_ma60_file = f'{today}-FT.npy'
     title = '期货推送'
+    latest_time = None
     for name, v in dict.items():
         ts_code = f'{v[0]}.{v[1]}'
         ma60 = get_ma60(ts_code, today, saved_ma60_file)
@@ -85,18 +89,28 @@ def main():
         ts_code = f'{v[0].lower()}'
         url = f'http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesMiniKLine5m?symbol={ts_code}'
         latest_5min = get_latest_5min(url)
-        isPush = calc(ma60, latest_5min)
+        if latest_5min is None:
+            print('get  latest_5min failed')
+            continue
+        if latest_time is None:
+            latest_time = latest_5min[0]
+        isPush, _ = calc(ma60, latest_5min)
         if isPush:
-            msg = f'期货:{name}－{ts_code} 五分钟线超过了60日线！'
+            msg = f'期货:{name}－{ts_code} 五分钟线超过了60日线！,{_}'
             pushWechat(title, msg)
+    return latest_time
 
 
 if __name__ == '__main__':
-    from threading import Timer
-
+    latest_time = None
     # 记录当前时间
     while True:
-        print(f'now:{datetime.datetime.now()}')
-        sTimer = Timer(60 * 5, main)
-        sTimer.start()
-        sTimer.join()
+        now = datetime.datetime.now()
+        delta = 0
+        if latest_time is not None:
+            latest_datetime = datetime.datetime.strptime(latest_time, '%Y-%m-%d %H:%M:%S')
+            days = (now - latest_datetime).days
+            if days > 0:
+                delta = (now - latest_datetime).seconds / 60
+        if latest_time is None or delta >= 5:
+            latest_time = main()
